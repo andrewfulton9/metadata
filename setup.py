ecc27bfd2e3a8b42a405814316fc2780ca7998d1
andrewfulton9 <andrewfulton9@gmail.com>
2025-05-05 15:45:03 -0600

    debugging

--- setup.py
+++ setup.py
@@ -127,6 +127,7 @@ setup(
     namespace_packages=[],
     install_requires=[
         'absl-py>=0.9,<3.0.0',
+        'build',
         'googleapis-common-protos>=1.56.4,<2;python_version>="3.11"',
         'protobuf>=4.25.2;python_version>="3.11"',
         'protobuf>=4.21.6,<4.22;python_version<"3.11"',
